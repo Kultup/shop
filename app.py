843dffc2fd25e73476fe58cd45eaa0657944dddb
kultup <gorodok048@gmail.com>
2025-11-11 15:25:41 +0200

    New UI changes: buttons, styles, photo switcher

--- app.py
+++ app.py
@@ -165,6 +165,6 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    # Для локального запуску (python app.py)
+    # Для локального запуску (python3 app.py або python app.py)
     app.run(debug=True, host='0.0.0.0', port=5000)
 
